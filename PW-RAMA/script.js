d5a36710443f89ef7a16262cc2de27af0325f81c
andreuador <andreusoriano2@gmail.com>
2023-09-12 17:15:57 +0200

    javascript

--- PW-RAMA/script.js
+++ PW-RAMA/script.js
@@ -1,2 +1,26 @@
 
-alert("Bienvenido a nuestra página web");
+//alert("Bienvenido a nuestra página web");
+
+//let valor = confirm("Mensaje con acción a confirmar");
+
+// Pide al usuario que ingrese su nombre.
+//let nombre = prompt("Por favor, ingrese tu nombre");
+
+// Verifica si el usuario ingreso un nombre o dejo el campo vacio.
+/*if (nombre == null || nombre == "") {
+    alert("Error: NO ingresaste tu nombre");
+}
+else {
+    let encabezado = document.createElement("h1");
+    encabezado.textContent = "Bienvenido, " + nombre;
+
+    // Agrega el encabezado a la parte superior derecha de la pagina
+    encabezado.style.position = "fixed";
+    encabezado.style.top = "10px";
+    encabezado.style.right = "10px";
+    //document.body.appendChild(encabezado);
+}
+
+var pelota = new Object();
+pelota.color = "Rojo";
+pelota.material = "plastico";*/
