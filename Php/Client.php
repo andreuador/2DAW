b5ea0c40dd6db20af3f05bde4dc26ca3331cb2df
andreuador <andreusoriano2@gmail.com>
2023-09-30 13:19:56 +0200

    PHP

---
+++ Php/Client.php
@@ -0,0 +1,52 @@
+<?php
+
+    class Client {
+        private array $nomClient;
+        private array $nomUsuari;
+        private array $email;
+        private string $dni;
+        private array $direccio;
+        private string $targeta;
+
+
+        public function __construct() {
+            $this->nomClient=["Pedro","Manolo","Pepito","Juan","Antonio","Luís"];
+            $this->nomUsuari=["Tron","Machupichu","Pepinillo","Slash","Slim","Alfalfa"];
+            $this->email=["a@hotmail.com","b@hotmail.com","c@hotmail.com","d@hotmail.com","d@hotmail.com","e@hotmail.com"];
+            $this->dni= $this->crearDNI();
+            $this->direccio=["La Vega","Fontanejos","Alcala","Puerta del Sol","Les Fonts","Brooklin"];
+            $this->targeta=rand(000000000000,999999999999);
+        }
+
+        public function crearDNI(){
+        return $this->randNums(8) .$this->randLetter(1);
+        }
+        public function generarNom():string{
+            return $this->nomClient[array_rand($this->nomClient)];
+        }
+        public function generarUsuari():string{
+            return $this->nomUsuari[array_rand($this->nomUsuari)];
+        }
+        public function generarEmail():string{
+            return $this->email[array_rand($this->email)];
+        }
+        public function generarDNI():string{
+            return $this->dni;
+        }
+        public function generarDireccio():string{
+            return $this->direccio[array_rand($this->direccio)];
+        }
+        public function generarTargeta():string{
+            return $this->targeta;
+        }
+
+        //Genera Els numeros i la lletra de dni
+        function randNums($i) : string{
+            return rand(00000000,99999999);
+        }
+        function randLetter($i) : string{
+            $lletres = chr(rand(65,90));
+            return $lletres;
+        }
+    }
+?>
